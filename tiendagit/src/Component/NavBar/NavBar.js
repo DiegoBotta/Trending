dc204e63f189dae06d85b52e27d21cb8d310226e
Diego Botta <diego.kpanga@hotmail.com>
2021-05-19 22:56:35 -0300

    Update NavBar.js
    Entregable Menu e-commerce

--- tiendagit/src/Component/NavBar/NavBar.js
+++ tiendagit/src/Component/NavBar/NavBar.js
@@ -12,17 +12,10 @@ export default function App() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
- 
+      <Nav.Link href="#home">Bicicletas completas</Nav.Link>
+      <Nav.Link href="#link">Componentes</Nav.Link>
+      <Nav.Link href="#link2">Nutricion</Nav.Link>
+    
     
     </Nav>
 
